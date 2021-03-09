761afca51df44c15527687695e7dacfb3916fe85
AnastasGeshovski <76570980+AnastasGeshovski@users.noreply.github.com>
2021-03-09 02:24:43 +0100

    Add files via upload

---
+++ Exercises____HOF_Anastas_Geshovski/script/app.js
@@ -0,0 +1,29 @@
+// // **** Exercise No. 1 ******* //
+
+// const arrayOfStrings = ["Anastas", "Robert", "Anja", "Slavko", "Ivo"];
+
+// arrayOfStrings.forEach((element, index) => {
+//     console.log(index + ' - ' + element)
+// });
+
+
+// // **** Exercise No. 2 ******* //
+
+// const arrayOfStuff = [1, 6, true, "Maja", "Rade"]
+
+// const arrayInput = (arrayOfStuff) => {console.log(arrayOfStuff.join(` `))}
+
+
+// arrayInput(arrayOfStuff);
+
+
+// // ***** Exercise No. 3 ***** //
+
+// const arrayStrings = ["Martin", "Sara", "Ivan", "Angela", "Ivo", "Robert"];
+// const newArr = [];
+
+// const someFunction = arrayStrings.filter(function (arrayStrings){
+//     return newArr.push(arrayStrings)
+// })
+// console.log(newArr);
+
